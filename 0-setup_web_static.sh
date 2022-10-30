68981bb80840fe01f1d9057f5c4a44ff8cc29813
root <root@DESKTOP-0I7I2F9.localdomain>
2022-10-30 10:41:59 +0100

    Bash script that sets up your web servers for the deployment of web_static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# automated configuration of web server for web static
+
+# install nginx if it doesn't exist
+if ! command -v nginx &> /dev/null
+then
+    sudo apt-get -y update
+    sudo apt-get -y install nginx
+fi
+
+mkdir /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/tests/
+
+echo "<p>Test<p>" > /data/web_static/releases/test/index.html
+
+ln -sf /data/web_static/releases/test /data/web_static/current
+
+chown -R ubuntu:ubuntu /data
+
+sed -i '53i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+
+service nginx restart
