a2cf6cd1c1fb8331ff0ae90c0bef6f34adfe32d4
root <root@DESKTOP-0I7I2F9.localdomain>
2022-11-01 22:26:31 +0100

    create a script to archive web_static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,15 @@
+# generates a .tgz archive from the contents of the web_static folder
+from fabric.api import local
+import time
+
+
+def do_pack():
+    """ returns .tgz archive if generated else None """
+
+    local('mkdir -p versions')
+    time_created = time.strftime("%Y%m%d%H%M%S")
+    arch_path = 'versions/web_static_{}.tgz'.format(time_created)
+
+    if local('tar -cvzf {} web_static'.format(arch_path)):
+        return arch_path
+    return None
